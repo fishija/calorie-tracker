4277709a877448bfea27c3caa0353d3c907a2b41
Jakub Mikula <cubamic@wp.pl>
2026-07-06 01:08:56 +0200

    Add MealForm

---
+++ app/meals/forms.py
@@ -0,0 +1,20 @@
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed, MultipleFileField
+from wtforms import StringField, TextAreaField, IntegerField, SubmitField
+from wtforms.validators import DataRequired
+
+
+class MealForm(FlaskForm):
+    name = StringField("Name", validators=[DataRequired()])
+    calorie_kcal = IntegerField("Calories (kcal)", validators=[DataRequired()])
+    protein_g = IntegerField("Proteins (g)", validators=[DataRequired()])
+    carb_g = IntegerField("Carbohydrates (g)", validators=[DataRequired()])
+    fat_g = IntegerField("Fats (g)", validators=[DataRequired()])
+    
+    description = TextAreaField("Description")
+    photos = MultipleFileField(
+        "Meal Photos",
+          validators=[FileAllowed(["jpg", "jpeg", "png"], "Images only")]
+    )
+
+    submit = SubmitField("Add Meal")
